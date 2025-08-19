313526a3d1d3d81d16f92030c5d715bea0231528
Rabin Tiwari <rabintiwari45@gmail.com>
2025-08-19 17:22:07 +0000

    add reward function for arc

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -44,6 +44,9 @@ def default_compute_score(
         from . import gsm8k
 
         res = gsm8k.compute_score(solution_str, ground_truth)
+    elif data_source == "barc":
+        from . import arc
+        res = arc.compute_score(solution_str, ground_truth)
     elif data_source in ["lighteval/MATH", "DigitalLearningGmbH/MATH-lighteval", "HuggingFaceH4/MATH-500"]:
         from . import math
 
---
+++ verl/utils/reward_score/arc.py
@@ -0,0 +1,46 @@
+
+
+import re
+
+
+def extract_solution(solution_str):
+
+    # Optimization: Regular expression matching on very long strings can be slow.
+    # For math problems, the final answer is usually at the end.
+    # We only match on the last 300 characters, which is a safe approximation for 300 tokens.
+        # this also tests the formatting of the model
+    solutions = re.findall("```(.*?)```", solution_str, re.DOTALL)
+    if len(solutions) == 0:
+        final_answer = None
+    else:
+        # take the last solution
+        final_answer = solutions[0]
+    return final_answer
+
+
+def compute_score(solution_str, ground_truth, score=1.0):
+    """The scoring function for GSM8k.
+
+    Reference: Trung, Luong, et al. "Reft: Reasoning with reinforced fine-tuning." Proceedings of the 62nd Annual
+    Meeting of the Association for Computational Linguistics (Volume 1: Long Papers). 2024.
+
+    Args:
+        solution_str: the solution text
+        ground_truth: the ground truth
+        score: the score for the correct answer
+    """
+    answer = extract_solution(solution_str=solution_str)
+    if answer is None:
+        return 0
+    else:
+        if answer.strip() == ground_truth.strip():
+            return score
+        else:
+            return 0
+
+
+
+
+
+
+
